93c4a7ef77ba366d857626231713c5ae3d2f15ed
starmun <abdemuqsit@hotmail.com>
2022-06-01 21:28:22 +0500

    Enabled swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OneByte.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<OneByteDbContext>();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.Run();
