a82a0b60947aaa49c156917e98aac4a3500f366f
starmun <abdemuqsit@hotmail.com>
2022-06-02 17:26:43 +0500

    Implemented token endpoint (login)

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,6 +1,10 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using OneByte.Models.DTO;
 
 namespace OneByte.Controllers
@@ -23,6 +27,27 @@ namespace OneByte.Controllers
             var user = new IdentityUser { UserName = userRegisterDto.Username };
             var result = await _userManager.CreateAsync(user, userRegisterDto.Password);
             return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(UserRegisterDto userRegisterDto)
+        {
+            var user = await _userManager.FindByNameAsync(userRegisterDto.Username);
+            var result = await _userManager.CheckPasswordAsync(user, userRegisterDto.Password);
+            if (result)
+            {
+                var token = new JwtSecurityToken(
+                    issuer: "OneByte",
+                    audience: "OneByte",
+                    claims: new[] { new Claim(JwtRegisteredClaimNames.Sub, user.UserName) },
+                    expires: DateTime.UtcNow.AddDays(7),
+                    signingCredentials: new SigningCredentials(new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("kIoAeLFFzuJwJDGTfv4BBQg0QMQfkFH7ALBaOFrBu528ixZBb3bJ6mbRansUgoD")), SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+            }
+
+            return Ok();
         }
     }
 }
